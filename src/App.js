538b3bd905b09d7f167a97cd61d593e05460d396
Hugh <hugh.wimberly@gmail.com>
2018-01-19 14:49:42 -0800

    Move state to App.js so that it is available to the search page.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import * as BooksAPI from "./BooksAPI";
 import "./App.css";
 import SearchPage from "./SearchPage";
 import ShelvesPage from "./ShelvesPage";
 
 class BooksApp extends React.Component {
-  state = {};
+  state = {
+    books: []
+  };
+
+  moveBook = (book, shelf) => {
+    BooksAPI.update(book, shelf)
+      .then(() =>
+        this.setState((prevState, props) => ({
+          books: prevState.books.map(b => {
+            if (b.id === book.id) {
+              b.shelf = shelf;
+            }
+            return b;
+          }).filter(b => b.shelf !== "none")
+        })))
+      .catch(response => console.error(`Unable to set shelf for ${book.id} to ${shelf}`, response));
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then(books => this.setState(() => ({ books })))
+      .catch(response => console.error("Unable to get booklist from server", response));
+  }
 
   render() {
     return (
       <div className="app">
-        <Route exact path="/" component={ShelvesPage} />
-        <Route path="/search" component={SearchPage} />
+        <Route exact path="/" render={() => <ShelvesPage books={this.state.books} moveBook={this.moveBook} />} />
+        <Route path="/search" render={() => <SearchPage books={this.state.books} moveBook={this.moveBook} />} />
       </div>
     );
   }
--- src/ShelvesPage.js
+++ src/ShelvesPage.js
@@ -1,58 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import * as BooksAPI from "./BooksAPI";
 import Bookshelf from "./Bookshelf";
 import * as Tags from "./util/tags.js";
 
-class ShelvesPage extends React.Component {
-  state = {
-    books: []
-  };
-
-  moveBook = (book, shelf) => {
-    BooksAPI.update(book, shelf)
-      .then(() =>
-        this.setState((prevState, props) => ({
-          books: prevState.books.map(b => {
-            if (b.id === book.id) {
-              b.shelf = shelf;
-            }
-            return b;
-          }).filter(b => b.shelf !== "none")
-        })))
-      .catch(response => console.error(`Unable to set shelf for ${book.id} to ${shelf}`, response));
-  }
-
-  componentDidMount() {
-    BooksAPI.getAll()
-      .then(books => this.setState(() => ({ books })))
-      .catch(response => console.error("Unable to get booklist from server", response));
-  }
-
-  render() {
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          {Tags.shelfNames.map(({ name, tag }) => (
-            <Bookshelf
-              key={tag}
-              shelfName={name}
-              moveBook={this.moveBook}
-              books={this.state.books.filter(book => tag === book.shelf)}
-            />
-          ))}
-        </div>
-        <div className="open-search">
-          <Link to="/search" className="close-search">
-            Add a book
-          </Link>
-        </div>
+function ShelvesPage(props) {
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
       </div>
-    );
-  }
+      <div className="list-books-content">
+        {Tags.shelfNames.map(({ name, tag }) => (
+          <Bookshelf
+            key={tag}
+            shelfName={name}
+            moveBook={props.moveBook}
+            books={props.books.filter(book => tag === book.shelf)}
+          />
+        ))}
+      </div>
+      <div className="open-search">
+        <Link to="/search" className="close-search">
+          Add a book
+          </Link>
+      </div>
+    </div>
+  );
 }
 
 export default ShelvesPage;
