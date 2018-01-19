ba8bff602db386669719ea677f202036b530877e
Hugh <hugh.wimberly@gmail.com>
2018-01-19 13:11:15 -0800

    Select option from <select> tag rather than <option> tag to follow React best practices.

--- src/ShelfChanger.js
+++ src/ShelfChanger.js
@@ -3,23 +3,15 @@ import * as Tags from "./util/tags.js";
 
 function ShelfChanger(props) {
   function onSelect(e) {
-    console.log(e);
+    console.log(e.target);
   }
 
   return (
     <div className="book-shelf-changer">
-      <select onChange={onSelect}>
-        <option value="none" disabled>
-          Move to...
-        </option>
-        {Tags.shelfNames.map(({ name, tag }) => (
-          <option key={tag} value={tag} selected={tag === props.book.shelf}>
-            {name}
-          </option>
-        ))}
-        <option value="none" selected={!props.book.shelf}>
-          None
-        </option>
+      <select onChange={onSelect} value={props.book.shelf}>
+        <option value="none" disabled>Move to...</option>
+        {Tags.shelfNames.map(({ name, tag }) => <option key={tag} value={tag}>{name}</option>)}
+        <option value="none">None</option>
       </select>
     </div>
   );
